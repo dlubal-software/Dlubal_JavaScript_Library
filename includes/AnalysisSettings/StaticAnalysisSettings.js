70111c4302faefa198c9b4aa72cbb9b8befb53ea
PavlikovaK <katerina.pavlikova@dlubal.com>
2022-02-02 08:59:50 +0100

    correction

--- includes/AnalysisSettings/StaticAnalysisSettings.js
+++ includes/AnalysisSettings/StaticAnalysisSettings.js
@@ -12,7 +12,7 @@ function StaticAnalysisType(type) {
     SASType = "GEOMETRICALLY_LINEAR";
     console.log("Wrong static analysis type input. Value was: " + type);
     console.log("Correct values are: ('linear', 'second order', 'large deformations')");
-  };
+  }
   console.log("Static analysis type: " + SASType);
   return SASType;  
 }
@@ -52,9 +52,9 @@ function NonlinearMethods(type, method) {
       console.log("Wrong nonlinear analysis method input. Value was: " + method);
       console.log("Correct values are: ( " + Object.keys(method_dict) + ")");
     };
-  };
+  }
   return nonlinear_method;  
-};
+}
 
 
 function SetEquationSolver(solverType) {
@@ -69,9 +69,9 @@ function SetEquationSolver(solverType) {
     console.log("Wrong equation solver input. Value was: " + solverType);
     console.log("Correct values are: ( " + Object.keys(EquationSolver_dict) + ")");
     equationSolver = "METHOD_OF_EQUATION_SYSTEM_DIRECT";
-  };
+  }
   return equationSolver 
-};
+}
 
 
 // Main function
@@ -102,21 +102,21 @@ function StaticAnalysisSettings(no,
     }
     else {
     	var SAS = static_analysis_settings.create(no);
-	};
+	}
   console.log("New static analysis settings no. " + SAS.no + " was created");
   // Static anlysis settings : type
   SAS.analysis_type = static_analysis_settings[StaticAnalysisType(analysisType)];
   if (equationSolver != undefined) {
     SAS.method_of_equation_system = static_analysis_settings[SetEquationSolver(equationSolver)];
-  };
+  }
   // Nonlinear method
   if (nonlinearMethod != undefined) {
     var NA_method = NonlinearMethods(SAS.analysis_type, nonlinearMethod);
     if (NA_method != undefined) {
       SAS.iterative_method_for_nonlinear_analysis = static_analysis_settings[NA_method];
       console.log("Nonlinear analysis method: " + SAS.iterative_method_for_nonlinear_analysis);
-    };
-  };
+    }
+  }
 	// Static analysis settings
 	this.settings = SAS;
   set_comment_and_parameters(this.settings, comment, params);
@@ -124,7 +124,7 @@ function StaticAnalysisSettings(no,
   // object for creation new static analysis settings with callback link to instance
   var self = this;
   return self;
-};
+}
 
 
 function AvoidWrongAssignment(SAS, param) {
@@ -137,9 +137,9 @@ function AvoidWrongAssignment(SAS, param) {
   }
   else {
   setParameter = true;
-  };
+  }
   return setParameter;
-};
+}
 
 
 StaticAnalysisSettings.prototype.Linear = function(equationSolver) {
@@ -147,7 +147,7 @@ StaticAnalysisSettings.prototype.Linear = function(equationSolver) {
   this.settings.analysis_type = static_analysis_settings.GEOMETRICALLY_LINEAR;
   if (equationSolver != undefined) {
     this.settings.method_of_equation_system = static_analysis_settings[SetEquationSolver(equationSolver)];
-  };
+  }
 };
 
 
@@ -157,17 +157,17 @@ StaticAnalysisSettings.prototype.SecondOrder = function(equationSolver, nonlinea
   this.settings.analysis_type = static_analysis_settings.SECOND_ORDER_P_DELTA;
   if (equationSolver != undefined) {
     this.settings.method_of_equation_system = static_analysis_settings[SetEquationSolver(equationSolver)];
-  };
+  }
   if (equationSolver != undefined) {
     this.settings.method_of_equation_system = static_analysis_settings[SetEquationSolver(equationSolver)];
-  };
+  }
   if (nonlinearMethod != undefined) {
     var NA_method = NonlinearMethods(this.settings.analysis_type, nonlinearMethod);
     if (NA_method != undefined) {
       this.settings.iterative_method_for_nonlinear_analysis = static_analysis_settings[NA_method];
       console.log("Nonlinear analysis method: " + this.settings.iterative_method_for_nonlinear_analysis);
-    };
-  };
+    }
+  }
 };
 
 
@@ -177,17 +177,17 @@ StaticAnalysisSettings.prototype.LargeDeformations = function(equationSolver, no
   this.settings.analysis_type = static_analysis_settings.LARGE_DEFORMATIONS;
   if (equationSolver != undefined) {
     this.settings.method_of_equation_system = static_analysis_settings[SetEquationSolver(equationSolver)];
-  };
+  }
   if (equationSolver != undefined) {
     this.settings.method_of_equation_system = static_analysis_settings[SetEquationSolver(equationSolver)];
-  };
+  }
   if (nonlinearMethod != undefined) {
     var NA_method = NonlinearMethods(this.settings.analysis_type, nonlinearMethod);
     if (NA_method != undefined) {
       this.settings.iterative_method_for_nonlinear_analysis = static_analysis_settings[NA_method];
       console.log("Nonlinear analysis method: " + this.settings.iterative_method_for_nonlinear_analysis);
-    };
-  };
+    }
+  }
 };
 
 
@@ -203,7 +203,7 @@ StaticAnalysisSettings.prototype.SetMaxNumberOfItrations = function(iterations) 
   if (AvoidWrongAssignment(this.settings, "max_number_of_iterations") === true) {   
     ASSERT(typeof iterations != undefined || typeof iterations != "number", "Parameter must be assigned as an integer.");
     this.settings.max_number_of_iterations = iterations;
-  };
+  }
 };
 
 
@@ -212,5 +212,5 @@ StaticAnalysisSettings.prototype.SetNumberOfLoadIncremets = function(increments)
 if (AvoidWrongAssignment(this.settings, "number_of_load_increments") === true) { 
   ASSERT(typeof increments != undefined || typeof increments != "number", "Parameter must be assigned as an integer.");
   this.settings.number_of_load_increments = increments;
-  };
+  }
 };
