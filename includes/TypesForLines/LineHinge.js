3bdce41a0b6218cacac3995546c9cded936651b2
PavlikovaK <katerina.pavlikova@dlubal.com>
2022-02-08 18:21:27 +0100

    Line hinges and line mesh refinement

--- includes/TypesForLines/LineHinge.js
+++ includes/TypesForLines/LineHinge.js
@@ -1,31 +1,160 @@
-function LineHinge(no,
-                   surface,
-                   lines,
-                   comment,
-                   params)
-{
-    var line_hinge = engine.create_line_hinge(no);
+// nonlinearity nefungují
 
-    if (typeof surface !== 'undefined')
-    {
-        if (typeof lines === 'undefined')
-        {
-            for (var i = 0; i < surface.boundary_lines.length; ++i)
-            {
-                surface.line_hinges_table[i + 1].line_number = surface.boundary_lines[i];
-                surface.line_hinges_table[i + 1].line_hinge = line_hinge;
-            }
-        }
-        else
-        {
-            for (var i = 0; i < lines.length; ++i)
-            {
-                surface.line_hinges_table[i + 1].line_number = lines[i];
-                surface.line_hinges_table[i + 1].line_hinge = line_hinge;
-            }
-        }
-    }
 
-    set_comment_and_parameters(line_hinge, comment, params);
-    return line_hinge;
+/**
+* Creates line hinge
+* @functiom
+* @constructor
+* @param	{Number}	no				Index of line hinge, can be undefined
+* @param	{String}	comment			Comment, can be undefined
+* @param	{Object}	params			line hinge parameters, can be undefined
+* @return	{Object}	Created line hinge
+*/
+var createLineHinge = function (no,
+								comment,
+								params) {
+
+	if (no != undefined) {
+		var lineHinge = line_hinges.create(no);
+	}
+	else {
+	 var lineHinge = line_hinges.create();
+	}
+	set_comment_and_parameters(lineHinge, comment, params);
+
+	return lineHinge;
+};
+
+
+/**
+* Creates line hinge constant
+* @functiom
+* @param	{Boolean or Float}	hinge		hinge input (true, false, number(stiffness))
+* @return	{Object}	Created hinge constant
+*/
+function CreateHingeConstant(hinge) {
+  var hinge_constant = line_hinges["SPRING_CONSTANT_NO"];
+
+  if (hinge === true) {
+    hinge_constant = line_hinges["SPRING_CONSTANT_YES"];
+  }
+  return hinge_constant;
 }
+
+
+function CreateHinge(hinge) {
+  if (hinge === true || hinge === false) {
+    return CreateHingeConstant(hinge);
+  }
+  else {
+    return hinge;
+  }
+}
+
+
+function CreateHingeVector(x, y, z) {
+  var hinge_vector = [x, y, z];
+  var hinge_vector_new = [0, 0, 0];
+  hinge_vector.forEach(function (hinge, index) {
+    hinge_vector_new[index] = CreateHinge(hinge);
+  });
+  return $V(hinge_vector_new[0], hinge_vector_new[1], hinge_vector_new[2]);
+}
+
+
+/**
+* Creates line hinge
+* @class
+* @constructor
+* @param	{Number}	no				Index of line hinge, can be undefined
+* @param	{String}	comment			Comment, can be undefined
+* @param	{Object}	params			line hinge parameters, can be undefined
+* @return	{Object}	Created line hinge
+*/
+function LineHinge(no,
+                   comment,
+                   params) {
+	this.lineHinge = createLineHinge(no, comment, params);
+	var self = this;
+	return self;
+}
+
+
+/**
+* Set translation constants ux, uy, uz to line hinge
+* @param	{Boolean or Float}	ux				Translation ux (true, false, number(stiffness [Nm^2]))
+* @param	{Boolean or Float}	uy				Translation uy (true, false, number(stiffness [Nm^2]))
+* @param	{Boolean or Float}	uz				Translation uz (true, false, number(stiffness [Nm^2]))
+* @return	{Object}	line hinge in parent
+*/
+LineHinge.prototype.Translation = function (ux, uy, uz) {
+	this.lineHinge.translational_release_u_x = CreateHinge(ux);
+	this.lineHinge.translational_release_u_y = CreateHinge(uy);
+	this.lineHinge.translational_release_u_z = CreateHinge(uz);
+	return this.lineHinge;
+};
+
+
+/**
+* Set translation constant ux to line hinge
+* @param	{Boolean or Float}	ux				Translation ux (true, false, number(stiffness [Nm^2]))
+* @return	{Object}	line hinge in parent
+*/
+LineHinge.prototype.TranslationX = function (ux) {
+	this.lineHinge.translational_release_u_x = CreateHinge(ux);
+	return this.lineHinge;
+};
+
+
+/**
+* Set translation constant uy to line hinge
+* @param	{Boolean or Float}	uy				Translation uy (true, false, number(stiffness [Nm^2]))
+* @return	{Object}	line hinge in parent
+*/
+LineHinge.prototype.TranslationY = function (uy) {
+	this.lineHinge.translational_release_u_y = CreateHinge(uy);
+	return this.lineHinge;
+};
+
+
+/**
+* Set translation constant uz to line hinge
+* @param	{Boolean or Float}	uz				Translation uz (true, false, number(stiffness [Nm^2]))
+* @return	{Object}	line hinge in parent
+*/
+LineHinge.prototype.TranslationZ = function (uz) {
+	this.lineHinge.translational_release_u_z = CreateHinge(uz);
+
+	return this.lineHinge;
+};
+
+
+/**
+* Set rotation constant rx to line hinge
+* @param	{Boolean or Float}	rx				Rotation rx (true, false, number(stiffness [Nm^2]))
+* @return	{Object}	line hinge in parent
+*/
+LineHinge.prototype.Rotation = function (rx) {
+	this.lineHinge.rotational_release_phi_x = CreateHinge(rx);
+	return this.lineHinge;
+};
+
+
+LineHinge.prototype.GetNo = function() {
+	return this.settings.no;
+}
+
+
+/**
+* Assign line hinge to line and surface (line must be involved in the surface)
+* @functiom
+* @param	{Integer}	table_id		Index of line_hinges_table (the range is the same as the amount of lines involved in the surface)
+* @param	{Integer}	line			line id for line hinge assign
+* @param	{Integer}	surface			surface id (line must lie on this surface)
+*/
+LineHinge.prototype.AssignTo = function(table_id, line, surface) {
+	var table = surfaces[surface].line_hinges_table[table_id];
+	table.line_hinge = this.lineHinge.no;
+	table.line_number = line;
+};
+
--- includes/TypesForLines/LineMeshRefinement.js
+++ includes/TypesForLines/LineMeshRefinement.js
@@ -1,8 +1,163 @@
+/**
+* Creates line mesh refinement
+* @functiom
+* @constructor
+* @param	{Number}			no						Index of line mesh refinement, can be undefined
+* @param	{Number}			target_length			Length of FEA element side on the line, can be undefined
+* @param	{Number}			layers					Number of layers to be affected by this refinement, can be undefined
+* @param	{Number or Array}	lines					Lines assigned to this refinement, can be undefined
+* @param	{String}			comment					Comment, can be undefined
+* @param	{Object}			params					line mesh refinement parameters, can be undefined
+* @return	{Object}			Created line mesh refinement
+*/
+var createLineMeshRefinement = function (no,
+										 target_length,											
+										 layers,
+										 lines,
+										 comment,
+										 params) {
+
+
+
+	if (no != undefined) {
+		var lineMeshRefinement = line_mesh_refinements.create(no);
+	}
+	else {
+	 	var lineMeshRefinement = line_mesh_refinements.create();
+	}
+
+	if (target_length != undefined) {
+		lineMeshRefinement.target_length = target_length;
+	}
+
+	if (layers != undefined) {
+		lineMeshRefinement.number_of_layers = layers;
+	}
+
+	if (lines != undefined) {
+		lineMeshRefinement.lines = lines;
+	}
+	set_comment_and_parameters(lineMeshRefinement, comment, params);
+
+
+	return lineMeshRefinement;
+};
+
+/**
+* Creates line mesh refinement
+* @class
+* @constructor
+* @param	{Number}			no						Index of line mesh refinement, can be undefined
+* @param	{Number}			target_length			Length of FEA element side on the line, can be undefined
+* @param	{Number}			layers					Number of layers to be affected by this refinement, can be undefined
+* @param	{Number or Array}	lines					Lines assigned to this refinement, can be undefined
+* @param	{String}			comment					Comment, can be undefined
+* @param	{Object}			params					line mesh refinement parameters, can be undefined
+* @return	{Object}			Created line mesh refinement HLF
+*/
 function LineMeshRefinement(no,
+							target_length,
+							layers,
+							lines,
                             comment,
                             params)
 {
-    var line_mesh_refinement = engine.create_line_mesh_refinement(no);
-    set_comment_and_parameters(line_mesh_refinement, comment, params);
-    return line_mesh_refinement;
+    this.settings = createLineMeshRefinement(no, target_length, layers, lines, comment, params);
+    var self = this;
+    return self;
 }
+
+
+/**
+* Change line mesh refinement to type based on element length
+* @param	{Number}			target_length			Length of FEA element side on the line, can be undefined
+* @param	{Number}			layers					Number of layers to be affected by this refinement, can be undefined
+* @param	{Number or Array}	lines					Lines assigned to this refinement, can be undefined
+* @return	{Object}			Created line mesh refinement
+*/
+LineMeshRefinement.prototype.Lenght = function(target_length, layers, lines) {
+	
+	this.settings.type = line_mesh_refinements.TYPE_LENGTH;
+	if (target_length != undefined) {
+		this.settings.target_length = target_length;
+	}
+
+	if (layers != undefined) {
+		this.settings.number_of_layers = layers;
+	}
+
+	if (lines != undefined) {
+		this.settings.lines = lines;
+	}
+
+	return this.settings
+}
+
+
+/**
+* Change line mesh refinement to type based on number of elements on line
+* @param	{Number}			elements			Number of FEA elements on the line, can be undefined
+* @param	{Number}			layers				Number of layers to be affected by this refinement, can be undefined
+* @param	{Number or Array}	lines				Lines assigned to this refinement, can be undefined
+* @return	{Object}			Created line mesh refinement
+*/
+LineMeshRefinement.prototype.Elements = function(elements, layers, lines) {
+
+	this.settings.type = line_mesh_refinements.TYPE_ELEMENTS;
+	if (elements != undefined) {
+		this.settings.elements_finite_elements = elements;
+	}
+
+	if (layers != undefined) {
+		this.settings.number_of_layers = layers;
+	}
+
+	if (lines != undefined) {
+		this.settings.lines = lines;
+	}
+	return this.settings
+}
+
+
+/**
+* Change line mesh refinement to type gradually changed due to distance from the line
+* @param	{Number}			gradual_rows			Number gradual rows of FEA elements, can be undefined
+* @param	{Number}			layers					Number of layers to be affected by this refinement, can be undefined
+* @param	{Number or Array}	lines					Lines assigned to this refinement, can be undefined
+* @return	{Object}			Created line mesh refinement
+*/
+LineMeshRefinement.prototype.Gradual = function(gradual_rows, layers, lines) {
+	this.settings.type = line_mesh_refinements.TYPE_GRADUAL;
+	if (gradual_rows != undefined) {
+		this.settings.gradual_rows = gradual_rows;
+	}
+
+	if (layers != undefined) {
+		this.settings.number_of_layers = layers;
+	}
+
+	if (lines != undefined) {
+		this.settings.lines = lines;
+	}
+	return this.settings
+
+}
+
+
+LineMeshRefinement.prototype.GetNo = function() {
+	return this.settings.no;
+}
+
+
+
+/**
+* Function for assign lines to line mesh refinement
+* @param	{Number}			gradual_rows			Number gradual rows of FEA elements, can be undefined
+* @param	{Number or Array}	lines					Lines assigned to this refinement, can be undefined
+* @return	{Object}			Created line mesh refinement
+*/
+LineMeshRefinement.prototype.SetLines = function(lines) {
+	this.settings.lines = lines;
+	return this.settings
+}
+	
