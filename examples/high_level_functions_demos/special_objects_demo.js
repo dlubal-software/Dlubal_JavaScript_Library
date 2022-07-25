193d231f144de56c9e593b67484435a474a15b7d
Jaroslav Broz <jara.broz@gmail.com>
2022-07-25 12:38:18 +0200

    Fix of test after fail in AT

--- examples/high_level_functions_demos/special_objects_demo.js
+++ examples/high_level_functions_demos/special_objects_demo.js
@@ -32,13 +32,13 @@ if (RFEM) {
 
 if (RFEM) {
 	var line = new Line();
-	for (var i = 0; i < nodesForLines.length; i+=2) {
+	for (var i = 0; i < nodesForLines.length; i += 2) {
 		line.Polyline(undefined, [nodesForLines[i].no, nodesForLines[i + 1].no]);
 	}
 }
 
 var member = new Member();
-for (var i = 0; i < nodesForMembers.length; i+=2) {
+for (var i = 0; i < nodesForMembers.length; i += 2) {
 	member.Beam(undefined, [nodesForMembers[i].no, nodesForMembers[i + 1].no], section.no);
 }
 
@@ -54,14 +54,14 @@ if (RFEM) {
 			memberHinge.Rotational(undefined, [membersForHinges[i].no], undefined, [true, 5000, 2], [true, 5000, 2], [true, 5000, 2]);
 		}
 	}
-	
+
 	var lineHinge = new LineHinge(1);
-	lineHinge.Translation(1500,2000,2500);
+	lineHinge.Translation(1500, 2000, 2500);
 	lineHinge.Rotation(false);
 	j = 21;
 	for (var i = 6; i <= 10; ++i) {
-		lineHinge.AssignTo(surfacesForLineHinges[i][0].no,j);
-		j += 4; 
+		lineHinge.AssignTo(surfacesForLineHinges[i][0].no, j);
+		j += 4;
 	}
 }
 
@@ -71,7 +71,7 @@ nodalSupport.Rotation(1.0, 1.5, 2.0);
 
 if (RFEM) {
 	var line = new Line();
-	for (var i = 0; i < nodesForLineSupports.length; i+=2) {
+	for (var i = 0; i < nodesForLineSupports.length; i += 2) {
 		line.Polyline(undefined, [nodesForLineSupports[i].no, nodesForLineSupports[i + 1].no]);
 		var lineSupport = new LineSupport();
 		lineSupport.SetLines([line.line.no]);
@@ -80,42 +80,51 @@ if (RFEM) {
 }
 
 var memberSupport = new MemberSupport();
-for (var i = 0; i < nodesForMembersForMemberSupports.length; i+=2) {
+for (var i = 0; i < nodesForMembersForMemberSupports.length; i += 2) {
 	member.Beam(undefined, [nodesForMembersForMemberSupports[i].no, nodesForMembersForMemberSupports[i + 1].no], section.no);
 	var memberSupport = new MemberSupport();
 	memberSupport.SetMembers([member.member.no]);
 	memberSupport.Shear(1100, 1200, 1300);
 }
 
-for (var i = 11; i <= 15; ++i) {
-	var surfaceSupport = new SurfaceSupport(undefined, surfacesForSupports[i][0].no);
-	surfaceSupport.Translation(200, 400, 800);
+if (RFEM) {
+	for (var i = 11; i <= 15; ++i) {
+		var surfaceSupport = new SurfaceSupport(undefined, surfacesForSupports[i][0].no);
+		surfaceSupport.Translation(200, 400, 800);
+	}
 }
 
 var memberStiffnessModification = new MemberStiffnessModification();
-var surfaceStiffnessModification = new SurfaceStiffnessModification();
-surfaceStiffnessModification.TotalStiffnessFactor(1, undefined, 2.5);
 
+if (RFEM) {
+	var surfaceStiffnessModification = new SurfaceStiffnessModification();
+	surfaceStiffnessModification.TotalStiffnessFactor(1, undefined, 2.5);
+}
 /*********************************************************************************************
 ****************************************** Main **********************************************
 *********************************************************************************************/
 var structureModification = new StructureModification(undefined, "First");
-structureModification.Material("Soda-lime silicate glass", undefined, 1.50, "Factor for E and G modification"); // 
+structureModification.Material("Soda-lime silicate glass", undefined, 1.50, "Factor for E and G modification"); //
 structureModification.Section("1/2 HEA 140 | 1 - S235", undefined, undefined, 1.50, undefined, 1.80); // Az and iY modification
 structureModification.Members(memberStiffnessModification.member_stiffness_modification.no, [6, 7], "Structure modification for members 1, 2");
-structureModification.Surfaces(surfaceStiffnessModification.surface_stiffness_modification.no, [1, 2, 3], "Structure modification for surfaces 1 - 3");
-structureModification.MemberHinges(6, "End", undefined, undefined, 1.50);	// translational spring constant Cu,z
-structureModification.MemberHinges(9, "Start", undefined, undefined, undefined, undefined, 1.50, 1.60);	// rotational spring constant Cφ,y and Cφ,z
-structureModification.LineHinges(6, 21, undefined, 1.6, 2.0); // Surface no. 6, line no. 21, Cu,x parameter set
-structureModification.LineHinges(7, 25, 1.1, 1.2); // Cu,x, Cu,y set
-structureModification.NodalSupports(62, undefined, undefined, undefined, 1.9, 2.0); // Node no. 62, Cφ,x, Cφ,y set
-structureModification.NodalSupports(70, undefined, undefined, undefined, undefined, undefined, 2.0); // Node no. 70, Cφ,z set
-structureModification.LineSupports(77, undefined, undefined, 2.0); // Line no. 77, Cu,z set
-structureModification.LineSupports(80, 1.1, 1.2, 1.3); // Line no. 80, Cu,x, Cu,y, Cu,z set
+if (RFEM) {
+	structureModification.Surfaces(surfaceStiffnessModification.surface_stiffness_modification.no, [1, 2, 3], "Structure modification for surfaces 1 - 3");
+	structureModification.MemberHinges(6, "End", undefined, undefined, 1.50);	// translational spring constant Cu,z
+	structureModification.MemberHinges(9, "Start", undefined, undefined, undefined, undefined, 1.50, 1.60);	// rotational spring constant Cφ,y and Cφ,z
+}
+if (RFEM) {
+	structureModification.LineHinges(6, 21, undefined, 1.6, 2.0); // Surface no. 6, line no. 21, Cu,x parameter set
+	structureModification.LineHinges(7, 25, 1.1, 1.2); // Cu,x, Cu,y set
+	structureModification.NodalSupports(62, undefined, undefined, undefined, 1.9, 2.0); // Node no. 62, Cφ,x, Cφ,y set
+	structureModification.NodalSupports(70, undefined, undefined, undefined, undefined, undefined, 2.0); // Node no. 70, Cφ,z set
+	structureModification.LineSupports(77, undefined, undefined, 2.0); // Line no. 77, Cu,z set
+	structureModification.LineSupports(80, 1.1, 1.2, 1.3); // Line no. 80, Cu,x, Cu,y, Cu,z set
+}
 structureModification.MemberSupports(12, undefined, undefined, undefined, 2.0, 2.5, 3.0); // Member no. 12, Cs,x, Cs,y, Cs,z set
-structureModification.SurfaceSupports(15, 1.0, 2.0, 3.0); // Surface no. 15, Cu,x, Cu,y, Cu,z set
-structureModification.DisableNonlinearitiesMemberHinges();
-
+if (RFEM) {
+	structureModification.SurfaceSupports(15, 1.0, 2.0, 3.0); // Surface no. 15, Cu,x, Cu,y, Cu,z set
+	structureModification.DisableNonlinearitiesMemberHinges();
+}
 var t2 = new Date().getTime();
 var time = (t2 - t1) / 1000;
 console.log("Elapsed time: " + time + "s");
